450df9c8274a8ac0171a57f02ade1dc560af2a9d
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-03-04 06:50:56 +0300

    add some js functions to mobile nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 const navBtn = document.querySelector(".nav-button");
 const mobileNav = document.querySelector(".mobile-nav");
+const body = document.body;
 
-navBtn.addEventListener("click", function () {
+navBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
   console.log("click");
-  mobileNav.classList.toggle("mobile-nav-active");
-  navBtn.classList.toggle("nav-button-close");
+  toggleNav();
 });
+
+window.addEventListener("click", function () {
+  if (body.classList.contains("no-scroll")) {
+    toggleNav();
+  }
+});
+
+mobileNav.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+function toggleNav() {
+  console.log("toggleNav");
+
+  body.classList.toggle("no-scroll");
+  navBtn.classList.toggle("nav-button-close");
+  mobileNav.classList.toggle("mobile-nav-active");
+}
